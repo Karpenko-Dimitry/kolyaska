f17a0b36b30b97a80c8787f6230394fabedcc73a
Karpenko Dimitry <karpenko.dimitry@gmail.com>
2022-08-10 21:32:14 +0300

    sm fix

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -76,7 +76,7 @@
 
 </footer>
 
-<script src="node_modules/jquery/dist/jquery.js"></script>
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
 <script src="/template/js/slider.js"></script>
 <script src="/template/js/modal.js"></script>
 <script src="/template/js/goods.js"></script>
